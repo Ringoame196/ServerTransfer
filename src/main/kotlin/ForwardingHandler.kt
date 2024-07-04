7d3d0b3b75d42ae33153a0a3e43bcfc44f0d32a9
りんご飴196 <kazumac41@gmail.com>
2024-07-04 18:59:22 +0900

    [修正] 無効なimportを削除

--- src/main/kotlin/ForwardingHandler.kt
+++ src/main/kotlin/ForwardingHandler.kt
@@ -3,60 +3,49 @@ package org.example
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
-import java.net.ServerSocket
 import java.net.Socket
-import javax.sound.sampled.Port
 
-class ForwardingHandler(private val clientSocket:Socket?,private val remoteHost:String?,private val remotePort:Int,private val localPort: Int):Runnable {
 
-    fun release() {
-        try {
-            ServerSocket(localPort).use { serverSocket ->
-                println("Listening on port $localPort")
-                while (true) {
-                    val clientSocket = serverSocket.accept()
-                    println("Client connected: " + clientSocket.inetAddress)
-
-                    // クライアントソケットとリモートソケットを処理する新しいスレッドを開始
-                    Thread(this).start()
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
+class ForwardingHandler(private val clientSocket:Socket, private val remoteHost:String, private val remotePort:Int):Runnable {
     override fun run() {
         try {
             Socket(remoteHost, remotePort).use { remoteSocket ->
-                clientSocket!!.getInputStream().use { clientIn ->
+                clientSocket.getInputStream().use { clientIn ->
                     clientSocket.getOutputStream().use { clientOut ->
                         remoteSocket.getInputStream().use { remoteIn ->
                             remoteSocket.getOutputStream().use { remoteOut ->
 
                                 // クライアントからリモートサーバーへのデータ転送
-                                Thread { forwardData(clientIn, remoteOut) }.start()
+                                Thread {
+                                    try {
+                                        forwardData(clientIn, remoteOut)
+                                    } catch (e: IOException) {
+                                        println("[サーバー転送] ${clientSocket.inetAddress} が切断しました")
+                                    }
+                                }.start()
+
                                 // リモートサーバーからクライアントへのデータ転送
-                                forwardData(remoteIn, clientOut)
+                                try {
+                                    forwardData(remoteIn, clientOut)
+                                } catch (e: IOException) {
+                                    println("[サーバー転送] ${clientSocket.inetAddress} が切断しました")
+                                }
                             }
                         }
                     }
                 }
             }
         } catch (e: IOException) {
-            e.printStackTrace()
+            println("[サーバー転送] ${clientSocket.inetAddress} が切断しました")
         }
     }
 
     private fun forwardData(`in`: InputStream, out: OutputStream) {
         val buffer = ByteArray(4096)
         var bytesRead: Int
-        try {
-            while ((`in`.read(buffer).also { bytesRead = it }) != -1) {
-                out.write(buffer, 0, bytesRead)
-                out.flush()
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
+        while (`in`.read(buffer).also { bytesRead = it } != -1) {
+            out.write(buffer, 0, bytesRead)
+            out.flush()
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
 package org.example
 
+import java.io.IOException
+import java.net.ServerSocket
+import java.net.SocketException
+
+
 fun main() {
-    val frameManager = FrameManager()
-    val frame = frameManager.make() // フレームを作る
-    frame.isVisible = true // フレームを表示する
+    val localPort = 19132 // 受信ポート
+    val remoteHost = "localhost" // 転送先のホスト
+    val remotePort = 25565 // 転送先のポート
+
+    try {
+        ServerSocket(localPort).use { serverSocket ->
+            println("[サーバー] サーバー起動しました (Port:${localPort})")
+            while (true) {
+                val clientSocket = serverSocket.accept()
+                println("[サーバー転送] ${clientSocket.inetAddress} -> ${remoteHost}:$remotePort")
+
+                // クライアントソケットとリモートソケットを処理する新しいスレッドを開始
+                Thread(ForwardingHandler(clientSocket, remoteHost, remotePort)).start()
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
 }

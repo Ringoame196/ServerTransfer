7282b32bd897773246483f8c98cb0ae8140db1da
りんご飴196 <kazumac41@gmail.com>
2024-07-05 21:17:41 +0900

    [実装]ログを取る機能追加

---
+++ src/main/kotlin/LogManager.kt
@@ -0,0 +1,33 @@
+package org.example
+
+import java.io.File
+import java.time.LocalDateTime
+
+class LogManager {
+    fun sendLog(log: String) {
+        makeLogFolder()
+        val time = LocalDateTime.now()
+        val hour = time.hour
+        val minute = time.minute
+        val second = time.second
+        val sendMessage = "[$hour:$minute.$second]$log"
+        println(sendMessage)
+        writeLogFile(sendMessage)
+    }
+    private fun writeLogFile(log:String) {
+        val time = LocalDateTime.now()
+        val year = time.year
+        val month = time.monthValue
+        val day = time.dayOfMonth
+        val filePath = "./log/$year-$month-$day.log"
+        val logFile = File(filePath)
+
+        logFile.appendText(log + "\n")
+    }
+    private fun makeLogFolder() {
+        val logFolderPath = "./log/"
+        if (!File(logFolderPath).exists()) {
+            File(logFolderPath).mkdirs()
+        }
+    }
+}
